5050143a04ef5914bb74106145ebef40caf17f7d
Yamila González <y.g.jadrosic@gmail.com>
2025-09-16 22:18:05 -0300

    agrego rutas home y login

--- app_reserva/src/main.js
+++ app_reserva/src/main.js
@@ -3,3 +3,21 @@ import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+    }
+    {
+        path: '/login',
+        name: 'Login',
+    }
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+createApp(App).use(router).mount('#app');
